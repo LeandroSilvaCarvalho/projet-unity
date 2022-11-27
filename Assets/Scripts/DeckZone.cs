ec26f90b85be19aba265348428a61e17524e9884
LeandroSilvaCarvalho <leandrosdc.pro@gmail.com>
2022-11-27 12:02:20 +0100

    unity files

--- Assets/Scripts/DeckZone.cs
+++ Assets/Scripts/DeckZone.cs
@@ -7,9 +7,11 @@ using Random = UnityEngine.Random;
 
 public class DeckZone : MonoBehaviour
 {
-
     [SerializeField] private List<GameObject> deck = new List<GameObject>();
     [SerializeField] private GameObject hand;
+    [SerializeField] private Transform[] cardSlots;
+    private bool[] availableCardsSlots;
+
     private void OnMouseDown()
     {
         DrawCard();
@@ -18,7 +20,11 @@ public class DeckZone : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        availableCardsSlots = new bool[cardSlots.Length];
+        for (int i = 0; i < availableCardsSlots.Length; i++)
+        {
+            availableCardsSlots[i] = true;
+        }
     }
 
     // Update is called once per frame
@@ -30,12 +36,22 @@ public class DeckZone : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Debug.Log("Clicked");
-            var toDraw = Random.Range(0, deck.Count-1);
-            Debug.Log(toDraw);
-            GameObject card = Instantiate(deck[toDraw],hand.transform);
-            deck.Remove(deck[toDraw]);
-            //card.transform.SetParent(hand.transform);
+            if (deck.Count >= 1)
+            {
+                var toDraw = Random.Range(0, deck.Count - 1);
+                Debug.Log(toDraw);
+                GameObject card = Instantiate(deck[toDraw], hand.transform);
+                for (int i = 0; i < availableCardsSlots.Length; i++)
+                {
+                    if (availableCardsSlots[i])
+                    {
+                        availableCardsSlots[i] = false;
+                        card.transform.position = cardSlots[i].transform.position;
+                        deck.Remove(deck[toDraw]);
+                        return;
+                    }
+                }
+            }
         }
     }
 }
